76efddf527b53b9ca5571aa9a497c6aec4997eb4
Dheeren Tejani <dheerennntejani@gmail.com>
2026-07-22 17:39:44 +0530

    fixed param issue v2

--- frontend/src/pages/Index.tsx
+++ frontend/src/pages/Index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useChatState } from '@/hooks/useChatState';
 import { ChatHeader } from '@/components/ChatHeader';
 import { ChatSidebar } from '@/components/ChatSidebar';
